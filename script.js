d167c6895b6e95ad30c8692b46ff751bcccf810f
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-15 01:52:24 +0500

    chap_5_done

---
+++ script.js
@@ -0,0 +1,73 @@
+//  chap no 5
+
+// unfamilar operator
+// increment++
+ let selray=150;
+ console.log(++selray);
+ 
+
+// let package=200;
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+console.log( ++package);
+
+let girl=60;
+console.log(++girl);
+this is pree increment
+
+let selary=300;
+const incrementedselary = selary++;
+console.log(incrementedselary);
+this is post increment
+
+Decrement--
+
+let charging=30;
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+console.log(--charging);
+
